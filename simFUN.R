f641ae59a17ac9b0e4907a5cec7341af814259cc
kevjosey <kevin.josey@ucdenver.edu>
2019-03-08 14:19:40 -0700

    added gen_data features.

--- simFUN.R
+++ simFUN.R
@@ -8,6 +8,56 @@ require(CBPS)
 require(ATE)
 require(survey)
 
+# simulate scenarios
+ks_data <- function(tau, n, sig2, rho, y_scen = c("a", "b"), z_scen = c("a", "b")) {
+  
+  # covariates
+  x1 <- stats::rnorm(n, 0, 1)
+  x2 <- stats::rnorm(n, 0, 1)
+  x3 <- stats::rnorm(n, 0, 1)
+  x4 <- stats::rnorm(n, 0, 1)
+  
+  # transformed predictors
+  u1 <- as.numeric(scale(exp(x1/2)))
+  u2 <- as.numeric(scale(x2/(1 + exp(x1)) + 10))
+  u3 <- as.numeric(scale((x1*x3/25 + 0.6)^3))
+  u4 <- as.numeric(scale((x2 + x4 + 20)^2))
+  
+  # treatment probabilities
+  if (z_scen == "b")
+    e_X <- 1/(1 + exp( -(-u1 + 0.5*u2 - 0.25*u3 - 0.1*u4) ) )
+  else
+    e_X <- 1/(1 + exp( -(-x1 + 0.5*x2 - 0.25*x3 - 0.1*x4) ) )
+  
+  r_exp <- stats::runif(n)
+  z <- ifelse(r_exp < e_X, 1, 0)
+  
+  # error variance
+  R <- matrix(rho, nrow = 2, ncol = 2)
+  diag(R) <- 1
+  V <- diag(sqrt(sig2), nrow = 2, ncol = 2)
+  Sig <- V %*% R %*% V
+  
+  if (y_scen == "b")
+    mu <- 210 + 27.4*u1 + 13.7*u2 + 13.7*u3 + 13.7*u4
+  else
+    mu <- 210 + 27.4*x1 + 13.7*x2 + 13.7*x3 + 13.7*x4
+  
+  eval <- eigen(Sig, symmetric = TRUE)
+  y_init <- matrix(stats::rnorm(n*2, 0, 1), nrow = n, ncol = 2) # iid potential outcomes
+  y_tmp <- t(eval$vectors %*% diag(sqrt(eval$values), nrow = 2) %*% t(y_init)) # SVD
+  y_pot <- y_tmp + cbind(mu, mu + tau) # include causal effect
+  
+  # observed outcome
+  y <- z*y_pot[,2] + (1 - z)*y_pot[,1]
+  
+  # create simulation dataset
+  sim <- as.data.frame(cbind(y, z, x1, x2, x3, x4, u1, u2, u3, u4))
+  
+  return(sim)
+  
+}
+
 # function for calculating inverse probabilty of treatment weights
 ipw <- function(ps, treat, estimand = c("ATE", "ATT"), standardize = TRUE) {
   
@@ -26,9 +76,9 @@ ipw <- function(ps, treat, estimand = c("ATE", "ATT"), standardize = TRUE) {
 }
 
 # Fits the balancing weights using a variety of methods
-simFit <- function(iter = 1, simDat) {
+simFit <- function(idx = 1, simDat) {
   
-  dat <- simDat[,iter]
+  dat <- simDat[,idx]
   formula <- as.formula(z ~ x1 + x2 + x3 + x4, env = environment(dat))
   cov_dat <- as.data.frame(dat[c("x1", "x2", "x3", "x4")])
   
@@ -60,10 +110,10 @@ simFit <- function(iter = 1, simDat) {
 }
 
 # wrapper for simEstimate()
-simPerf <- function(iter = 1, weightsList, simDat, tau, type = c("point", "se", "coverage")) {
+simPerf <- function(idx = 1, weightsList, simDat, tau, type = c("point", "se", "coverage")) {
   
-  data <- as.data.frame(simDat[,iter])
-  work <- as.data.frame(weightsList[,iter])
+  data <- as.data.frame(simDat[,idx])
+  work <- as.data.frame(weightsList[,idx])
   formula <- y ~ z
   
   est_tmp <- lapply(work, simEstimate, formula = formula, data = data, tau = tau, type = type)
